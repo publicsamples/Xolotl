2953fccf32dad232382f005ba9d76d5de4d15414
Rick <53375860+RickTaylor79@users.noreply.github.com>
2023-09-02 20:02:56 +0100

    UI update

--- Xolotl/Scripts/ScriptProcessors/Xolotl/Interface.js
+++ Xolotl/Scripts/ScriptProcessors/Xolotl/Interface.js
@@ -1,4 +1,4 @@
-Content.makeFrontInterface(905, 565);
+Content.makeFrontInterface(945, 605);
 
 
 include("factory_waves.js");
@@ -14,57 +14,22 @@ var Maps = [];
 Maps.push(BasicWaveforms);
 Maps.push(GM);
 Maps.push(Hybrids);
-Maps.push(Kits);
 Maps.push(cs30);
 Maps.push(em25);
 Maps.push(sys101);
 Maps.push(jx3p);
 Maps.push(kraftzwerg);
-Maps.push(XPFM);
-Maps.push(XPMisc);
-Maps.push(XPPWM);
-Maps.push(XPSaws);
-Maps.push(XPSquares);
+Maps.push(Xpander);
 Maps.push(Monopoly);
 
 var USERMaps = [];
 
 var USMAPS = ["USER"];
 
-var SMAPS = ["BasicWaveforms", "GM", "Hybrids", "Kits", "cs30", "em25", "sys101","jx3p", "kraftzwerg", "XPFM", "XPMisc","XPPWM", "XPSaws", "XPSquares", "Monopoly"];
+var SMAPS = ["BasicWaveforms", "GM", "Hybrids", "cs30", "em25", "sys101","jx3p", "kraftzwerg","Xpander", "Monopoly"];
 
 //var SMAPS = ["BasicWaveforms", "GM", "Hybrids", "Kits"];
 
-
-const var Mode = Content.getComponent("Mode");
-const var A = Content.getComponent("A");
-const var B = Content.getComponent("B");
-const var mode1 = Content.getComponent("mode1");
-const var A2 = Content.getComponent("A2");
-const var B1 = Content.getComponent("B1");
-
-
-inline function onModeControl(component, value)
-{
-	B.showControl(value); 
-	        A.showControl(1-value);
-
-HARMONIC.setAttribute(HARMONIC.userwav1, value);
-};
-
-Content.getComponent("Mode").setControlCallback(onModeControl);
-
-
-
-inline function onmode1Control(component, value)
-{
-	B1.showControl(value); 
-	        A2.showControl(1-value);
-	        
-	        HARMONIC.setAttribute(HARMONIC.userwav2, value);
-};
-
-Content.getComponent("mode1").setControlCallback(onmode1Control);
 
 
 
@@ -78,7 +43,7 @@ inline function onLoadSFZ1Control(component, value)
 	{
 		
 
-		slot1.loadFile("{XYZ::SFZ}" + (f.toString(File.FullPath)));
+		slot.loadFile("{XYZ::SFZ}" + (f.toString(File.FullPath)));
 		WAVELABEL1.set("text", "USER");
 	
 	});	
@@ -121,9 +86,6 @@ const var harm = Synth.getAudioSampleProcessor("HARMONIC");
                 
 const slot = harm.getAudioFile(0);
 const slot1 = harm.getAudioFile(1);
-const slot2 = harm.getAudioFile(2);
-const slot3 = harm.getAudioFile(3);
-
 
 const var HARMONIC = Synth.getChildSynth("HARMONIC")
 
