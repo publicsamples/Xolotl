da507bc26ca65d8a7d923830ad86bbf650fd2d04
Rick <53375860+RickTaylor79@users.noreply.github.com>
2026-05-05 11:03:53 +0100

    broken

--- Xolotl/Scripts/Samples.js
+++ Xolotl/Scripts/Samples.js
@@ -48,10 +48,14 @@ for (file in AudioList) {
 // Run the sorting function
 sortAudioFilesListV1();
 
+var g_sampleLoadToken = 0;
+
 
 inline function onSampleBankControl(component, value)
 {
-  if (value >= 0) {
+  if (value <= 0 || value > foldersV1.length)
+      return;
+
         // Get the selected Genre
         local selectedGenre = foldersV1[value-1];
      //   Console.print("Selected Genre: " + selectedGenre);
@@ -72,9 +76,9 @@ inline function onSampleBankControl(component, value)
 
         
      //   FirstCB.setValue(value);
-		SampleA.setValue(value);
+		// Do not force SampleA here. During preset restore this can overwrite
+		// the preset's SampleA value and trigger a load with half-restored state.
          
-    }
 }
 
 Content.getComponent("SampleBank").setControlCallback(onSampleBankControl);
@@ -82,14 +86,29 @@ Content.getComponent("SampleBank").setControlCallback(onSampleBankControl);
 
 
 inline function onSampleAControl(component, value)
-{if (value >= 0) {
+{
+	if (value <= 0)
+		return;
 
-		local selectedSample = SampleA.get("items").split("\n")[value - 1];
+		local sampleItems = SampleA.get("items").split("\n");
+
+		if (value > sampleItems.length)
+			return;
+
+		local selectedSample = sampleItems[value - 1];
+
+		if (selectedSample.length == 0 || selectedSample == "no file")
+			return;
 
        // Construct the full path to the sample
        local selectedGenre = SampleBank.getItemText();
-      // local selectedInstrument = SecondCB.getItemText();
+
+       if (selectedGenre.length == 0)
+           return;
+
        local fullPath = "{PROJECT_FOLDER}" + selectedGenre + "/" + selectedSample;
+       g_sampleLoadToken = g_sampleLoadToken + 1;
+       local loadToken = g_sampleLoadToken;
 
 	
 	SynthesiserGroup1.setBypassed(false);
@@ -97,26 +116,30 @@ inline function onSampleAControl(component, value)
 	
 	Content.callAfterDelay(300, function()
 	{
+		if (loadToken != g_sampleLoadToken)
+			return;
+
 		Engine.allNotesOff();
 	
 		SynthesiserGroup1.setBypassed(true);
 		
 		Content.callAfterDelay(300, function()
 		{
+			if (loadToken != g_sampleLoadToken)
+				return;
 	
 	
 		SynthesiserGroup1.setBypassed(false);
 
    
-        // Load the sample using the full path
-        slot1.loadFile("{PROJECT_FOLDER}" + SampleBank.getItemText()  + "/" + SampleA.getItemText());
+        // Load the sample using the validated path from callback time.
+        slot1.loadFile(fullPath);
       //  Console.print("Sample loaded successfully!");
     
     
     }, this);
 
 	}, ScriptnodeSynthesiser1);
-}
 }
 
 Content.getComponent("SampleA").setControlCallback(onSampleAControl);
@@ -139,11 +162,11 @@ const Maps = [];
 
 
 const BellKey = ["Bell_Belles", "Bell_More", "Bell_Src", "Keys_DCOPiano", "Keys_Darkly", "Keys_EM25_1", "Keys_EM25_2", "Keys_EM25_3", "Keys_EM25_4", "Keys_EP1", "Keys_EP2", "Keys_EP3", "Keys_Euro", "Keys_Odd", "Keys_Swoosh1", "Keys_Swoosh2", "Keys_Swoosh3", "Keys_pswp"];
-const FX = ["FX_CrazyLFO", "FX_Devistate", "FX_Drama", "FX_FMRings", "FX_FeedFwd", "FX_Gooey", "FX_HiYearner", "FX_LickElectric", "FX_LoopG", "FX_MagicResin", "FX_S&H", "FX_Zap", "FX__Bypass", "FX_heartache", "FX_BeastMode", "FX_BrainSeq", "FX_BseqSmooth", "FX_Burd", "FX_SmearedToy", "FX_Swish", "FX_WowOhWow"];
-const KitSeq = ["Kit_612_perc", "Kit_CS", "Kit_MP", "Kit_Sh2", "SEQ_Bumpkin130", "SEQ_SpaceRaider", "SEQ_TwistingMellow1", "SEQ_TwistingMellow2", "SEQ_revolve130", "SEQ_sale", "Seq_Lemon90", "Seq_consider"];
+const FX = ["FX_CrazyLFO", "FX_Devistate", "FX_Drama", "FX_FMRings", "FX_FeedFwd", "FX_Gooey", "FX_HiYearner", "FX_LickElectric", "FX_LoopG", "FX_MagicResin", "FX_S&H", "FX_Zap", "FX_Bypass", "FX_heartache", "FX_BeastMode", "FX_BrainSeq", "FX_BseqSmooth", "FX_Burd", "FX_SmearedToy", "FX_Swish", "FX_WowOhWow"];
+const KitSeq = ["Kit_612_perc", "Kit_CS", "Kit_MP", "Kit_SH2", "SEQ_Bumpkin130", "SEQ_SpaceRaider", "SEQ_TwistingMellow1", "SEQ_TwistingMellow2", "SEQ_revolve130", "SEQ_sale", "Seq_Lemon90", "Seq_consider"];
 const Lead = ["Lead_2ndFinger", "Lead_Astonish", "Lead_Astonished", "Lead_Bargain", "Lead_Basic", "Lead_Brew", "Lead_Cream", "Lead_CrumbFlute", "Lead_Crunch", "Lead_Crunchy", "Lead_CupCake", "Lead_Curious", "Lead_DF1", "Lead_Day3", "Lead_Drink", "Lead_Egg", "Lead_FakeSync", "Lead_Fingers", "Lead_Grizzle", "Lead_K30L", "Lead_K_Legs", "Lead_Log", "Lead_Lozange", "Lead_Messin", "Lead_PseudoSync", "Lead_RavST", "Lead_Rodge", "Lead_SadRing", "Lead_Sayer", "Lead_SaysThings", "Lead_Scream", "Lead_Screamer", "Lead_ShRes", "Lead_Simple", "Lead_SimpleJx", "Lead_SlwRing1", "Lead_SlwRing2", "Lead_SlwRing3", "Lead_TSync", "Lead_Tinkle", "Lead_funny", "Lead_harsh", "Lead_k30l2", "Lead_mth", "Lead_rude", "Lead_sawd", "Lead_stix"];
 const Misc = ["Misc_EM25_1", "Misc_EM25_2", "Misc_EM25_3", "Misc_Gdad_1", "Misc_Gdad_2", "Misc_Gdad_3", "Misc_Gdad_4", "Misc_Gdad_5", "Misc_Meta_1", "Misc_Meta_2", "Misc_Meta_3", "Misc_Meta_4", "Misc_Meta_5", "Misc_SplGit", "Misc_Spook", "Misc_Toes", "Misc_Vibe", "Misc_WBrass", "Misc_Wet", "Pluck_Gater", "Pluck_PluckSH", "Bass_BooBass", "Bass_Bz", "Bass_Double", "Bass_Gooey", "Bass_HardBass", "Bass_HellCat", "Bass_Lilbass", "Bass_RM", "Bass_Round", "Bass_ShakeyBass", "Bass_Shrp", "Bass_Slap", "Bass_eBass", "Bass_hard", "Bass_nsty1", "Bass_nsty2"];
-const Pad = ["Pad_BigElectric", "Pad_Bumpy", "Pad_Busy", "Pad_CsShock", "Pad_DF2", "Pad_DS", "Pad_Gravel", "Pad_GreatST", "Pad_Lfade", "Pad_Lingo", "Pad_OChord", "Pad_PulseString", "Pad_RLD", "Pad_Rouse", "Pad_Rust", "Pad_Scratchy", "Pad_Scrd", "Pad_SlowOne", "Pad_Special", "Pad_Swish", "Pad_Tortoise", "Pad_Trouble", "Pad_XpSea", "Pad_simr", " Strings_Classy", "Strings_EM25_1", "Strings_EM25_2", "Strings_EM25_3", "Strings_Z1_1", "Strings_Z1_2"];
+const Pad = ["Pad_BigElectric", "Pad_Bumpy", "Pad_Busy", "Pad_CsShock", "Pad_DF2", "Pad_DS", "Pad_Gravel", "Pad_GreatST", "Pad_Lfade", "Pad_Lingo", "Pad_OChord", "Pad_PulseString", "Pad_RLD", "Pad_Rouse", "Pad_Rust", "Pad_Scratchy", "Pad_Scrd", "Pad_SlowOne", "Pad_Special", "Pad_Swish", "Pad_Tortoise", "Pad_Trouble", "Pad_XpSea", "Pad_simr", "Strings_Classy", "Strings_EM25_1", "Strings_EM25_2", "Strings_EM25_3", "Strings_Z1_1", "Strings_Z1_2"];
 const Waves = ["Wave_MP4xSaw1", "Wave_MP4xSaw2", "Wave_MPSawA1", "Wave_MPSawA2", "Wave_MPSawA3", "Wave_MPSawA4", "Wave_MPSawB1", "Wave_MPSawB2", "Wave_MPSawB3", "Wave_MPSqr_1", "Wave_MPSqr_2", "Wave_MPSqr_3", "Wave_MPSqr_4", "Wave_MPSqr_5", "Wave_MPTri", "Wave_Noise", "Wave_Noise1", "Wave_Noise2", "Wave_Noise3", "Wave_Noise4", "Wave_Pulse1", "Wave_Pulse2", "Wave_Pulse3", "Wave_Saw", "Wave_Sine"];
 
 
@@ -169,8 +192,48 @@ const var Categories = Content.getComponent("Categories");
 
 Categories.set("items", SMAPS.join("\n"));
 
+var g_pendingSampleMapId = "";
+var g_isSampleMapLoadScheduled = false;
+var g_lastLoadedSampleMapId = "";
+
+inline function isValidSampleMapId(id)
+{
+	return id.length > 0 && maps.indexOf(id) != -1;
+}
+
+inline function requestSampleMapLoad(mapId)
+{
+	if (!isValidSampleMapId(mapId))
+		return;
+
+	g_pendingSampleMapId = mapId;
+
+	if (g_isSampleMapLoadScheduled)
+		return;
+
+	g_isSampleMapLoadScheduled = true;
+
+	Content.callAfterDelay(50, function()
+	{
+		local id = g_pendingSampleMapId;
+		g_pendingSampleMapId = "";
+		g_isSampleMapLoadScheduled = false;
+
+		if (!isValidSampleMapId(id))
+			return;
+
+		if (id == g_lastLoadedSampleMapId)
+			return;
+
+		g_lastLoadedSampleMapId = id;
+		slot.loadFile("{XYZ::SampleMap}" + id);
+	}, WAVELABEL1);
+}
+
 inline function onCategoriesControl(component, value)
 {
+	if (value <= 0 || value > Maps.length)
+		return;
 
 	BankA.set("items", [].join("\n")); 
 	BankA.set("items", Maps[value-1].join("\n"));
@@ -184,7 +247,12 @@ Content.getComponent("Categories").setControlCallback(onCategoriesControl);
 
 inline function onWAVELABEL1Control(component, value)
 {
-slot.loadFile("{XYZ::SampleMap}" + WAVELABEL1.get("text"));
+	local mapId = WAVELABEL1.get("text");
+
+	if (!isValidSampleMapId(mapId))
+		return;
+
+	requestSampleMapLoad(mapId);
 };
 
 Content.getComponent("WAVELABEL1").setControlCallback(onWAVELABEL1Control);
@@ -195,9 +263,22 @@ Content.getComponent("WAVELABEL1").setControlCallback(onWAVELABEL1Control);
 
 inline function onBankAControl(component, value)
 {
-	WAVELABEL1.set("text",BankA.get("items").split("\n")[value-1]);
+	if (value <= 0)
+		return;
+
+	local bankItems = BankA.get("items").split("\n");
+
+	if (value > bankItems.length)
+		return;
+
+	local mapId = bankItems[value-1];
+
+	if (!isValidSampleMapId(mapId))
+		return;
+
+	WAVELABEL1.set("text", mapId);
 	
-	slot.loadFile("{XYZ::SampleMap}" + component.getItemText());
+	requestSampleMapLoad(mapId);
 //	slot.loadFile("{XYZ::SampleMap}" + (maps[value-1]));
 };
 
@@ -229,4 +310,3 @@ inline function onWaveLoadControl(component, value)
 
 
 Content.getComponent("WaveLoad").setControlCallback(onWaveLoadControl);
-
